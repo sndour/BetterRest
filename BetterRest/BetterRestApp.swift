6610a3274950f27bd3514b00fd57f062d467ad5f
sndour <ndouradefemi@gmail.com>
2023-01-02 21:39:26 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Stéphane Adefemi NDOUR on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
